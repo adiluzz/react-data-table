bb4483f64c67a89e3fc0d7ba979e081577863b92
adiluzz <adiluzz@gmail.com>
2024-06-30 16:46:06 +0300

    typescript now works

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 // import EsLint from 'vite-plugin-linter'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import * as packageJson from './package.json'
+import tsConfigPaths from 'vite-tsconfig-paths';
+import * as packageJson from './package.json';
+
 // const { EsLinter, linterPlugin } = EsLint
 // https://vitejs.dev/config/
 // export default defineConfig((configEnv) => ({
