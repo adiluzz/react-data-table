13fa6e12ed25df51dbfb61440566dec218a0e4c4
adiluzz <adiluzz@gmail.com>
2024-07-01 15:04:59 +0300

    Changed indentation

--- src/examples/ApiUsage.tsx
+++ src/examples/ApiUsage.tsx
@@ -4,66 +4,66 @@ import { TableField } from '../DataTable.interface';
 import './App.css';
 
 type User = {
-	id: string;
-	name: string;
-	username: string;
+    id: string;
+    name: string;
+    username: string;
 }
 
 
 function App() {
-	const [users, setUsers] = useState<User[]>();
-	const removeUserFromTable = (id: string) => {
-		if (users) {
-			setUsers(
-				users.filter((user) => {
-					return user?.id && user.id !== id;
-				})
-			);
-		}
-	};
-	const tableFields: TableField<User>[] = [
-		{ key: "id", headerText: "ID", sortable: true, groupable: true },
-		{ key: "name", headerText: "Name", sortable: true, groupable: true },
-		{ key: "username", headerText: "Username", sortable: true, groupable: true },
-		{
-			renderComponent: (row) => {
-				return (
-					<button
-						onClick={() => {
-							if (row.id) {
-								removeUserFromTable(row.id);
-							}
-						}}
-					>
-						Delete {row.name}
-					</button>
-				);
-			},
-			headerText: "Delete",
-			key: "delete",
-		},
-	];
+    const [users, setUsers] = useState<User[]>();
+    const removeUserFromTable = (id: string) => {
+        if (users) {
+            setUsers(
+                users.filter((user) => {
+                    return user?.id && user.id !== id;
+                })
+            );
+        }
+    };
+    const tableFields: TableField<User>[] = [
+        { key: "id", headerText: "ID", sortable: true, groupable: true },
+        { key: "name", headerText: "Name", sortable: true, groupable: true },
+        { key: "username", headerText: "Username", sortable: true, groupable: true },
+        {
+            renderComponent: (row) => {
+                return (
+                    <button
+                        onClick={() => {
+                            if (row.id) {
+                                removeUserFromTable(row.id);
+                            }
+                        }}
+                    >
+                        Delete {row.name}
+                    </button>
+                );
+            },
+            headerText: "Delete",
+            key: "delete",
+        },
+    ];
 
-	const fetchUserData = useCallback(async () => {
-		const res = await fetch("https://jsonplaceholder.typicode.com/users");
-		const users: User[] = await res.json();
-		const duplicatedUsers: User[] = [...users, ...users.map((user, i) => {
-			return { ...user, id: String(11 + i) }
-		})]
+    const fetchUserData = useCallback(async () => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        const users: User[] = await res.json();
+        const duplicatedUsers: User[] = [...users, ...users.map((user, i) => {
+            return { ...user, id: String(11 + i) }
+        })]
 
-		setUsers(duplicatedUsers);
-	}, []);
+        setUsers(duplicatedUsers);
+    }, []);
 
 
-	useEffect(() => {
-		fetchUserData();
-	}, [fetchUserData]);
+    useEffect(() => {
+        fetchUserData();
+    }, [fetchUserData]);
 
-	return (
-		<div className="App">
-			{users && <DataTable data={users} fields={tableFields} />}
-		</div>
-	);
+    return (
+        <div className="App">
+            {users && <DataTable data={users} fields={tableFields} />}
+        </div>
+    );
 }
 
 export default App
